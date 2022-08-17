0d1b23f4fffbeb48386f77d64583861348bc2e10
Yuliya Sidorova <sidorovayuliya883@gmail.com>
2022-08-17 12:39:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    static {
+        int amount = 20000;
+        {
+
+        }
+        int miles = 20;
+
+        var bonus = amount / miles;
+
+        System.out.println(bonus);
+
+
+    }
+
+
+}
